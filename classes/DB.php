ceab755031670d4e423a2b4ffb7604a58f6ca84e
r0808 <arthurdelophem3@gmail.com>
2022-03-24 15:10:35 +0100

    fix: db connection

--- classes/DB.php
+++ classes/DB.php
@@ -1,18 +1,19 @@
 <?php
-include_once(__DIR__ .'/../bootstrap.php');
+include_once(__DIR__ . '/../bootstrap.php');
 
 abstract class DB
 {
     private static $conn;
 
-    public static function getConnection(){
+    public static function getConnection()
+    {
         if (self::$conn) {
             //connection found
             return self::$conn;
         } else {
             //no connection found
-            $config = parse_ini_file(__DIR__.'/../config/config.ini');
-            self::$conn = new PDO("mysql:host=".$config['server'].";dbname=".$config['database'], $config['username'], $config['password']);
+            $config = parse_ini_file(__DIR__ . '/../config/config.ini');
+            self::$conn = new PDO("mysql:host=" . $config['server'] . ";dbname=" . $config['database'], $config['user'], $config['password']);
             return self::$conn;
         }
     }
