fa3de8f1eab567f4b32fdbf2fda65ac27bd4deb8
Robbe Bierebeeck <77198548+RobbeBierebeeck@users.noreply.github.com>
2022-04-08 12:02:30 +0200

    feat: add form and styling, custom tag feeld is also added

--- feed.php
+++ feed.php
@@ -3,7 +3,7 @@
 include_once(__DIR__ . '/helpers/Security.php');
 include_once(__DIR__ . '/bootstrap.php');
 Security::onlyLoggedInUsers();
-$profileImg = User::getProfilePicture();
+echo User::getProfilePicture();
 
 ?>
 <!DOCTYPE html>
@@ -21,75 +21,92 @@ $profileImg = User::getProfilePicture();
 </head>
 
 <body>
-    <nav class="navbar navbar-light bg-light fixed-top">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-                <img src="images/logo.svg" alt="" width="30" height="24" class="d-inline-block align-text-top">
-                Drop
-            </a>
-            <form>
-                <input class="form-control" type="search" placeholder="Search" aria-label="Search">
-            </form>
-            <div class="d-flex align-items-center">
-                <div class="dropdown">
-                    <img class="avatar avatar-48 bg-light rounded-circle text-white p-2 dropdown-toggle" id="dropdownMenuButton1" data-toggle="dropdown" aria-haspopup="true" data-bs-toggle="dropdown" aria-expanded="false" role="button" src="<?php echo $profileImg ?>">
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Profiel</a></li>
-                        <li><a class="dropdown-item" href="accountSettings.php">Instellingen</a></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li><a class="dropdown-item" href="logout.php">Afmelden</a></li>
-                    </ul>
-                </div>
-                <i class="bi bi-bell fs-5 me-2"></i>
-                <button type="button" class="btn btn-primary">Drop your shot</button>
-                </img>
 
+<nav class="navbar navbar-light bg-light fixed-top">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="images/logo.svg" alt="" width="30" height="24" class="d-inline-block align-text-top">
+            Drop
+        </a>
+        <form>
+            <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+        </form>
+        <div class="d-flex align-items-center">
+            <div class="dropdown">
+                <img class="avatar avatar-48 bg-light rounded-circle text-white p-2 dropdown-toggle" id="dropdownMenuButton1" data-toggle="dropdown" aria-haspopup="true" data-bs-toggle="dropdown" aria-expanded="false" role="button" src="<?php echo User::getProfilePicture()?>">
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Profiel</a></li>
+                    <li><a class="dropdown-item" href="accountSettings.php">Instellingen</a></li>
+                    <li>
+                        <hr class="dropdown-divider">
+                    </li>
+                    <li><a class="dropdown-item" href="logout.php">Afmelden</a></li>
+                </ul>
             </div>
-    </nav>
-    <div class="row row-cols-1 row-cols-md-2 g-4">
-        <div class="col">
-            <div class="card">
-                <img src="images/dribblePost.webp" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional
-                        content. This content is a little bit longer.</p>
-                </div>
-            </div>
+            <i class="bi bi-bell fs-5 me-2"></i>
+            <button type="button" class="btn btn-primary">Drop your shot</button>
+            </img>
+
         </div>
-        <div class="col">
-            <div class="card">
-                <img src="image/dribblePost.webp" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional
-                        content. This content is a little bit longer.</p>
+</nav>
+<div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4 mt-5 container-lg">
+    <div class="col mt-4">
+        <div class="card">
+            <img src="images/post_template.webp" class="card-img-top" alt="...">
+            <div class="card-body d-flex flex-row justify-content-between">
+                <h5 class="card-title">Card title</h5>
+                <div class="d-flex">
+                    <p class="me-2"> 400 <i class="bi bi-eye"></i> </p>
+                    <a> 100 <i class="bi bi-star"></i></a>
                 </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card">
-                <img src="image/dribblePost.webp" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional
-                        content.</p>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card">
-                <img src="image/dribblePost.webp" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional
-                        content. This content is a little bit longer.</p>
-                </div>
+
             </div>
         </div>
     </div>
+    <div class="col mt-4">
+        <div class="card">
+            <img src="images/post_template.webp" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+
+            </div>
+        </div>
+    </div>
+    <div class="col mt-4">
+        <div class="card">
+            <img src="images/post_template.webp" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+
+            </div>
+        </div>
+    </div>
+    <div class="col mt-4">
+        <div class="card">
+            <img src="images/post_template.webp" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+
+            </div>
+        </div>
+    </div>
+    <div class="col mt-4">
+        <div class="card">
+            <img src="images/post_template.webp" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+            </div>
+        </div>
+    </div>
+    <div class="col mt-4">
+        <div class="card">
+            <img src="images/post_template.webp" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+            </div>
+        </div>
+    </div>
+</div>
     <script src="js/bootstrap.bundle.js"></script>
 </body>
 
---
+++ newPost.php
@@ -0,0 +1,53 @@
+<?php
+?><!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>New post</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+<div class="container vw-100 vh-100 d-flex justify-content-center align-items-center">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Ready to drop a post?</h1>
+            <form action="savePost.php" method="post">
+                <div class="form-group mt-3">
+                    <label for="formFile" class="form-label">Drop your shot</label>
+                    <input class="form-control" type="file" id="formFile">
+                </div>
+                <!-- Tag input feeld -->
+                <div class="mt-3">
+                    <label class="form-label" for="floatingInput">Tags</label>
+                    <div class="input form-control" id="floatingInput">
+                        <div class="input__tags"></div>
+                        <input type="text" id="tags">
+                    </div>
+                </div>
+                <input type="text" name="tags" id="tags-fake">
+                <!-- Title input feeld -->
+                <div class="form-group mt-3">
+                    <label for="title">Title</label>
+                    <input type="text" class="form-control" id="title" name="title">
+                </div>
+                <!-- Content input feeld -->
+                <div class="form-group mt-3">
+                    <label for="content">Content</label>
+                    <textarea class="form-control" id="content" name="content" rows="3"></textarea>
+                </div>
+
+
+
+                <!-- Submit button -->
+                <button type="submit" class="btn btn-primary mt-3">Drop it</button>
+            </form>
+        </div>
+    </div>
+    <script src="scripts/tags.js"></script>
+</body>
+</html>
