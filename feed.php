c19689f76f4ebd23bf6abe605ba085603eacc209
Lukas <Lukas@MacBook-Pro-9.local>
2022-04-10 14:06:43 +0200

    Fix: style feed page

--- feed.php
+++ feed.php
@@ -20,7 +20,9 @@ $profileImg = User::getProfilePicture($_SESSION['user']);
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="style/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-avatar@latest/dist/avatar.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+            crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -36,9 +38,11 @@ $profileImg = User::getProfilePicture($_SESSION['user']);
         </form>
         <div class="d-flex align-items-center">
             <div class="dropdown">
-                <img class="avatar avatar-48 bg-light rounded-circle text-white p-2 dropdown-toggle" id="dropdownMenuButton1" data-toggle="dropdown" aria-haspopup="true" data-bs-toggle="dropdown" aria-expanded="false" role="button" src="<?php echo $profileImg?>">
+                <img class="avatar avatar-48 bg-light rounded-circle text-white p-2 dropdown-toggle"
+                     id="dropdownMenuButton1" data-toggle="dropdown" aria-haspopup="true" data-bs-toggle="dropdown"
+                     aria-expanded="false" role="button" src="<?php echo $profileImg ?>">
                 <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Profiel</a></li>
+                    <li><a class="dropdown-item" href="profile.php">Profiel</a></li>
                     <li><a class="dropdown-item" href="accountSettings.php">Instellingen</a></li>
                     <li>
                         <hr class="dropdown-divider">
@@ -51,64 +55,66 @@ $profileImg = User::getProfilePicture($_SESSION['user']);
         </div>
     </div>
 </nav>
-<div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4 mt-5 container-lg">
-    <div class="col mt-4">
-        <div class="card">
-            <img src="images/post_template.webp" class="card-img-top" alt="...">
-            <div class="card-body d-flex flex-row justify-content-between">
-                <h5 class="card-title">Card title</h5>
-                <div class="d-flex">
-                    <p class="me-2"> 400 <i class="bi bi-eye"></i> </p>
-                    <a> 100 <i class="bi bi-star"></i></a>
+<div>
+        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4 m-auto mt-5 container-lg">
+            <div class="col mt-4">
+                <div class="card">
+                    <img src="images/post_template.webp" class="card-img-top" alt="...">
+                    <div class="card-body d-flex flex-row justify-content-between">
+                        <h5 class="card-title">Card title</h5>
+                        <div class="d-flex">
+                            <p class="me-2"> 400 <i class="bi bi-eye"></i></p>
+                            <a> 100 <i class="bi bi-star"></i></a>
+                        </div>
+
+                    </div>
                 </div>
+            </div>
+            <div class="col mt-4">
+                <div class="card">
+                    <img src="images/post_template.webp" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
 
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-    <div class="col mt-4">
-        <div class="card">
-            <img src="images/post_template.webp" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
+            <div class="col mt-4">
+                <div class="card">
+                    <img src="images/post_template.webp" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
 
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-    <div class="col mt-4">
-        <div class="card">
-            <img src="images/post_template.webp" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
+            <div class="col mt-4">
+                <div class="card">
+                    <img src="images/post_template.webp" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
 
+                    </div>
+                </div>
+            </div>
+            <div class="col mt-4">
+                <div class="card">
+                    <img src="images/post_template.webp" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col mt-4">
+                <div class="card">
+                    <img src="images/post_template.webp" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-    <div class="col mt-4">
-        <div class="card">
-            <img src="images/post_template.webp" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-
-            </div>
-        </div>
-    </div>
-    <div class="col mt-4">
-        <div class="card">
-            <img src="images/post_template.webp" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-            </div>
-        </div>
-    </div>
-    <div class="col mt-4">
-        <div class="card">
-            <img src="images/post_template.webp" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-            </div>
-        </div>
-    </div>
-</div>
     <script src="js/bootstrap.bundle.js"></script>
 </body>
 
