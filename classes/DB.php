612d9e58c73ff3687721a03c4f34e3b51272054e
Robbe Bierebeeck <77198548+RobbeBierebeeck@users.noreply.github.com>
2022-04-10 18:57:02 +0200

    feat: pdo fetch assoc is now the default fetchmode

--- classes/DB.php
+++ classes/DB.php
@@ -14,6 +14,7 @@ abstract class DB
             //no connection found
             $config = parse_ini_file(__DIR__ . '/../config/config.ini');
             self::$conn = new PDO("mysql:host=" . $config['server'] . ";dbname=" . $config['database'], $config['username'], $config['password']);
+            self::$conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
             return self::$conn;
         }
     }
