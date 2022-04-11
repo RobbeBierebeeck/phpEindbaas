145ac79b2715e58cb3234426180a596b72a24dc5
Lukas <Lukas@macbook-pro-9.home>
2022-04-11 15:04:26 +0200

    Style: add forms to edit profile page

--- account/editProfile.php
+++ account/editProfile.php
@@ -5,9 +5,6 @@ Security::onlyLoggedInUsers();
 $profileImg = User::getProfilePicture($_SESSION['user']);
 $id = User::getUserId($_SESSION['user']);
 $userData = User::getById($id);
-if ($_POST['delete']){
-    User::removeUser($id);
-}
 ?>
 <!doctype html>
 <html lang="en">
@@ -92,10 +89,35 @@ if ($_POST['delete']){
                 <div class="e-navlist e-navlist--active-bg">
                     <div class="d-flex flex-row">
                         <img class="avatar avatar-96 bg-light rounded-circle text-white p-2 dropdown-toggle" id="dropdownMenuButton1" data-toggle="dropdown" aria-haspopup="true" data-bs-toggle="dropdown" aria-expanded="false" role="button" src="./.<?php echo $profileImg?>">
-                        <form id="profile" class="mb-0" action="" method="post">
-                            <input type="submit" class="btn btn-primary" name="profile" value="Change profile picture">
+                        <form id="profile" class="mb-0 d-flex justify-content-center align-items-center" action="" method="post">
+                            <input type="submit" class="btn btn-primary ms-5" name="profile" value="Change profile picture">
+                            <input type="submit" class="btn btn-light ms-3" name="delete" value="Delete picture">
                         </form>
                     </div>
+
+                    <form action="" class="" method="post">
+                        <div class="mb-5">
+                            <label class="mb-1" for="name">Name</label>
+                            <div class="form">
+                                <input type="text" name="name" class="form-control p-3" id="oldPassword" placeholder="" value="<?php echo $userData['firstname']?> <?php echo $userData['lastname']?>" required>
+                            </div>
+                        </div>
+                        <div class="mb-5 mt-3">
+                            <label for="biography">Bio</label>
+                            <div class="form">
+                                <textarea class="form-control" name="biography" id="biography" rows="3"></textarea>
+                            </div>
+                        </div>
+                        <div class="mb-5">
+                            <label class="mb-1" for="secondEmail">Secondary email</label>
+                            <div class="form">
+                                <input type="text" name="secondEmail" class="form-control p-3" id="secondEmail" placeholder="Fill in an email adress" value="" required>
+                            </div>
+                        </div>
+                        <div class="mb-3">
+                            <button type="submit" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
