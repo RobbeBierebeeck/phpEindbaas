bf9ad2d94d845e83235b4ede00a3f552643b3d9a
r0808 <arthurdelophem3@gmail.com>
2022-04-21 23:50:19 +0200

    feat: add email box color validation

--- scripts/emailCheck.js
+++ scripts/emailCheck.js
@@ -5,19 +5,24 @@ document.querySelector(".email").addEventListener("change", () => {
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.status == 200) {
             let textFeedback = document.querySelector(".email-validator");
+            let box = document.querySelector(".email");
             textFeedback.innerHTML = xmlhttp.responseText;
 
             if (xmlhttp.responseText == "this email is already in use") {
                 textFeedback.classList.add("invalid-feedback");
                 textFeedback.style.display = "inline-block";
+                box.classList.add("is-invalid");
             }
             if (xmlhttp.responseText == "email available") {
                 textFeedback.classList.add("valid-feedback");
                 textFeedback.style.display = "inline-block";
+                box.classList.add("is-valid");
             }
             if (mailBoxValue == "") {
                 textFeedback.classList.remove("invalid-feedback");
                 textFeedback.classList.remove("valid-feedback");
+                box.classList.remove("is-valid");
+                box.classList.remove("is-invalid");
                 textFeedback.style.display = "none";
             }
         }
