cf3fc8bd513a668b3eab6ecdcad4f4a16b7ca47f
r0808 <arthurdelophem3@gmail.com>
2022-04-29 15:42:02 +0200

    feat: add check for thomasmore email + visual feedback

--- scripts/emailCheck.js
+++ scripts/emailCheck.js
@@ -8,34 +8,42 @@ document.querySelector(".email").addEventListener("change", () => {
     let textFeedback = document.querySelector(".email-validator");
     let box = document.querySelector(".email");
 
-    fetch(`./scripts/ajax/emailChecker.php`, {
-        method: 'POST',
-        body: data,
-    })
-        .then(response => response.json())
-        .then((data) => {
-            console.log(data.message);
-
-            textFeedback.innerHTML = data.message;
-
-            if (data.message == "this email is already in use") {
-                textFeedback.classList.add("invalid-feedback");
-                textFeedback.style.display = "inline-block";
-                textFeedback.classList.remove("valid-feedback");
-                box.classList.remove("is-valid");
-                box.classList.add("is-invalid");
-            }
-            if (data.message == "email available") {
-                textFeedback.classList.add("valid-feedback");
-                textFeedback.style.display = "inline-block";
-                textFeedback.classList.remove("invalid-feedback");
-                box.classList.add("is-valid");
-                box.classList.remove("is-invalid");
-            }
+    if (mailBoxValue.indexOf('@') > -1 && mailBoxValue.indexOf('.thomasmore.be') > -1) {
+        fetch(`./scripts/ajax/emailChecker.php`, {
+            method: 'POST',
+            body: data,
         })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
+            .then(response => response.json())
+            .then((data) => {
+                console.log(data.message);
+
+                textFeedback.innerHTML = data.message;
+
+                if (data.message == "this email is already in use") {
+                    textFeedback.classList.add("invalid-feedback");
+                    textFeedback.style.display = "inline-block";
+                    textFeedback.classList.remove("valid-feedback");
+                    box.classList.remove("is-valid");
+                    box.classList.add("is-invalid");
+                }
+                if (data.message == "email available") {
+                    textFeedback.classList.add("valid-feedback");
+                    textFeedback.style.display = "inline-block";
+                    textFeedback.classList.remove("invalid-feedback");
+                    box.classList.add("is-valid");
+                    box.classList.remove("is-invalid");
+                }
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    } else {
+        textFeedback.classList.add("invalid-feedback");
+        textFeedback.style.display = "inline-block";
+        textFeedback.innerHTML = "use your Thomas More email for registration";
+        box.classList.add("is-invalid");
+    }
+
     if (mailBoxValue.length == 0) {
         console.log("empty")
         textFeedback.style.display = "none";
