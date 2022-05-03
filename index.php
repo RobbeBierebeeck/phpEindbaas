048e8ef80abc9a0397fd7bcb82085669bbbc6bd5
r0808 <arthurdelophem3@gmail.com>
2022-05-03 19:15:29 +0200

    fix: get userId inside each post instead of getAll function

--- index.php
+++ index.php
@@ -73,7 +73,7 @@ if(!empty($_GET['search'])&&!empty($_GET['page'])){
                         </div>
                         <div class="list-group list-group-flush d-flex flex-row align-items-center">
                             <span class="rounded-circle nav__profilePicture ms-3 m-2" style="background-image: url('<?php echo $post['profile_image'] ?>');"></span>
-                            <span>by <a href="profile.php?id=<?php echo $post['id']; ?>" class="fw-bolder"><?php echo $post['firstname'] . " " . $post['lastname'] ?></a> </span> </span>
+                            <span>by <a href="profile.php?id=<?php echo Post::getCreatorByPost($post['id'])["id"]?>" class="fw-bolder"><?php echo $post['firstname'] . " " . $post['lastname'] ?></a> </span> </span>
                         </div>
                     </div>
                 </div>
