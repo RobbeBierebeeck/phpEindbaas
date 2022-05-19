d32f359928d55638d2d37bcd43caee35ac2ffedd
Robbe Bierebeeck <77198548+RobbeBierebeeck@users.noreply.github.com>
2022-05-19 14:41:35 +0200

    add viewport tag to index

--- index.php
+++ index.php
@@ -26,6 +26,7 @@ if (!empty($_GET['search']) && !empty($_GET['page'])) {
 
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bootstrap</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
